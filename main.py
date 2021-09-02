f2df8a101027acdcbba3afd9ac1eb70761977f40
Quang Blue <quangblue@Quangs-MacBook-Pro.local>
2021-09-02 21:47:39 +0700

    1st

---
+++ main.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+import json
+app = FastAPI()
+
+
+class LoginFshare (BaseModel):
+    user_email: str
+    password: str
+
+
+@app.post("/loginFshare")
+async def login_fshare(item: LoginFshare):
+    url = 'https://api.fshare.vn/api/user/login'
+    appKey = 'dMnqMMZMUnN5YpvKENaEhdQQ5jxDqddt'
+    userAgent = 'macflix-8TY6B8'
+
+    url = 'https://api.fshare.vn/api/user/login'
+    headers = {'user-agent': userAgent,
+               'content-type': 'application/json'}
+
+    payload = {'user_email': item.user_email,
+               'password':	item.password,
+               'app_key': appKey}
+
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+
+    return r.json()
