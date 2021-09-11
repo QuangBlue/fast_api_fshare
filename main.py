2a0147b3bd330a22d9e1ce5ab27912b14ae6611f
Quang Blue <quangblue1603@gmail.com>
2021-09-11 20:01:41 +0700

    update

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ async def login_fshare(item: LoginFshare):
                'content-type': 'application/json'}
 
     payload = {'user_email': item.user_email,
-               'password':	item.password,
+               'password':  item.password,
                'app_key': appKey}
 
     r = requests.post(url, data=json.dumps(payload), headers=headers)
@@ -80,9 +80,100 @@ async def info_fshare(item: FileFshare):
     r = requests.get(url)
     html = r.text
     soup = BeautifulSoup(html, 'html.parser')
+    try:
+        titleFile = soup.find("div", class_="name").attrs["title"]
+        sizeFile = soup.find_all(
+            "button", id='download-free')[1].text.strip().split(" | ")[1]
+    except:
+        return {'msg': 'File does not exist', 'code': 'error'}
+    typeFile = titleFile.split(".")[-1]  # mkv
+    # Justice.League.Snyders.Cut.2021.1080p.Untouched.HD.AVC.x264
+    nameFile = titleFile.split(f'.{typeFile}')[0]
 
-    titleFile = soup.find("div", class_="name").attrs["title"]
-    sizeFile = soup.find_all(
-        "button", id='download-free')[1].text.strip().split(" | ")[1]
-    json = {'title_file': titleFile, 'size_file': sizeFile}
-    return json.json()
+    listRes = ['2160p', '1080p', '720p']
+    resFile = 'other'
+
+    for res in listRes:
+        r = titleFile.find(res)
+        if r != -1:
+            resFile = res
+            break
+
+    encodeFile = ''
+    listEncode = ['hevc', '265']
+    listEncode1 = ['avc', '264']
+
+    for q in listEncode:
+        r = titleFile.lower().find(q)
+        if r != -1:
+            encodeFile = 'x265'
+            break
+
+    for q1 in listEncode1:
+        r = titleFile.lower().find(q1)
+        if r != -1:
+            encodeFile = 'x264'
+            break
+
+    typeAudio = ''
+    tAAC = titleFile.lower().find('aac')
+    if tAAC != -1:
+        typeAudio = 'AAC'
+
+    listAC3 = ['ddp', 'ac3', 'truehd', 'dd']
+
+    for ac3 in listAC3:
+        r = titleFile.lower().find(ac3)
+        if r != -1:
+            typeAudio = 'AC-3'
+            break
+    tDts = titleFile.lower().find('dts')
+    if tDts != -1:
+        typeAudio = 'DTS'
+
+    qualityFile = ''
+    listQuality = ['bluray', 'blu-ray']
+    for x in listQuality:
+        qBr = titleFile.lower().find(x)
+        if qBr != -1:
+            qualityFile = 'BR Encode'
+    qBr1 = titleFile.lower().find('remux')
+    if qBr1 != -1:
+        qualityFile = 'BR Remux'
+
+    listQ3 = ['web-dl', 'hdtv', 'webrip']
+    for x in listQ3:
+        qqq = titleFile.lower().find(x)
+        if qqq != -1:
+            qualityFile = x.upper()
+            break
+    channelFile = ''
+    listChannel = ['5.1', '7.1']
+    for x in listChannel:
+        qqq = titleFile.lower().find(x)
+        if qqq != -1:
+            channelFile = x
+            break
+    hdrFile = ''
+    hdr = titleFile.lower().find('hdr')
+    if hdr != -1:
+        hdrFile = 'HDR'
+
+    if typeFile == 'iso':
+        resFile = 'ISO'
+
+    json = {
+        'msg': 'File is exist',
+        'code': 'success',
+        'name': nameFile,
+        'type': typeFile,
+        'size': sizeFile,
+        'resolution': resFile,
+        'encode_type': encodeFile,
+        'audio_type': typeAudio,
+        'quality': qualityFile,
+        'audio_channel': channelFile,
+        'hdr': hdrFile,
+    }
+
+    return json
